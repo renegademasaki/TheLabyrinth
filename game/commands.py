7e123866bc2e09544147c70b0d110e39c19e11b2
Jeff Lundquist <jlcoolguy@gmail.com>
2024-11-17 21:31:53 +0000

    drop command works, examine is WIP

--- game/commands.py
+++ game/commands.py
@@ -16,8 +16,8 @@ class CommandParser:
       "inventory": self.inventory_command,
       "inv": self.inventory_command,
       "take": self.take_command,
-      #"drop": self.drop_command,
-      #"examine": self.examine_command,
+      "drop": self.drop_command,
+      "examine": self.examine_command,
       "use": self.use_command,
       #"talk": self.talk_command,
       #"give": self.give_command,
@@ -53,8 +53,8 @@ class CommandParser:
       "look": "Look around the current room",
       "inventory/inv": "Display the items in your inventory",
       "take <item>": "Pick up an item and add to your inventory",
-      #"drop <item>": "Drop an item from your inventory",
-      #"examine <item>": "Look closely at an item",
+      "drop <item>": "Drop an item from your inventory",
+      "examine <item>": "Look closely at an item",
       "use <item>": "Use an item in your inventory",
       "talk [choice]": "Talk to a character",
       "give <item>": "Give an item to a character",
@@ -151,7 +151,6 @@ class CommandParser:
     else:
       self.console.print("[red]You don't see that here.[/red]")
 
-  '''
   def drop_command(self, command, *args):
     """Handle dropping items"""
     if not args:
@@ -161,7 +160,7 @@ class CommandParser:
     item_name = " ".join(args)
     current_room = self.game_engine.player.current_room
 
-    for item in self.game_engine.player.inventory():
+    for item in self.game_engine.player.inventory:
       if item.name.lower() == item_name.lower():
         self.game_engine.player.remove_from_inventory(item)
         current_room.add_item(item)
@@ -180,7 +179,7 @@ class CommandParser:
     current_room = self.game_engine.player.current_room
 
     # Check inventory first
-    for item in self.game_engine.player.inventory():
+    for item in self.game_engine.player.inventory:
       if item.name.lower() == item_name.lower():
         self.console.print(f"\n[yellow]{item.name}[/yellow]")
         self.console.print(item.examine())
@@ -205,7 +204,6 @@ class CommandParser:
             self.console.print(f"- {content_item.name}")
     else:
       self.console.print("[red]You don't see that here.[/red]")
-      '''
 
   def quit_command(self, *args):
     """Exit the game"""
