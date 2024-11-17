255245c44e2d87c1df9f0bead2ac6951c395f725
Jeff Lundquist <jlcoolguy@gmail.com>
2024-11-17 16:35:40 +0000

    class Player, def move

--- game/player.py
+++ game/player.py
@@ -2,3 +2,11 @@ class Player:
   def __init__(self, starting_room):
     self.current_room = starting_room
     self.inventory = []
+
+  def move(self, direction):
+    """Attempt to move the player in the specified direction.
+    Returns True if the move was successful, False otherwise."""
+    if direction in self.current_room.exits:
+      self.current_room = self.current_room.exits[direction]
+      return True
+    return False
