3587708a0a1f74be2f2436811c427477146ed548
Jeff Lundquist <jlcoolguy@gmail.com>
2024-11-17 02:17:27 +0000

    deleted some test code

--- game/engine.py
+++ game/engine.py
@@ -53,23 +53,3 @@ class GameEngine:
         self.display_welcome()
         self.game_loop()
 
-
-
-#Some examples of how to use the engine, BE SURE TO DELETE THESE BEFORE SUBMITTING
-class Dog:
-  def __init__(self, name, breed):
-      self.name = name
-      self.breed = breed
-
-  def bark(self):
-      return f'{self.name} says Woof!'
-
-  def type(self):
-      return f'{self.name} is a {self.breed}'
-
-# Creating an instance of the Dog class
-my_dog = Dog('Rylie', 'Mini Dachshund')
-shawns_dog = Dog('Roman', 'Mastiff')
-
-def hello_world():
-  print('Hello World!')
--- main.py
+++ main.py
@@ -9,15 +9,3 @@ def main():
 if __name__ == '__main__':
   main()
 
-
-
-#Some examples of how to use the engine, BE SURE TO DELETE THESE BEFORE SUBMITTING
-game.engine.hello_world()
-
-print(game.engine.my_dog.type())
-print(game.engine.my_dog.bark())
-
-print(game.engine.shawns_dog.type())
-print(game.engine.shawns_dog.bark())
-
-print('This is a change')
